71ed56b30de5f6e731c668c84ae0c5b08a9ebf15
Erdem Akbaba <engineer.akbaba@gmail.com>
2023-10-04 18:05:21 +0300

    Add project files.

---
+++ Coin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coins
+{
+    public static class Coin
+    {
+        /// <summary>
+        /// Method definition to calculate how many ways are there to make change for a total amount of cents
+        /// </summary>
+        /// <param name="coinList"></param>
+        /// <param name="totalAmount"></param>
+        /// <returns></returns>
+        public static int CountChanges(int[] coinList, int toatlAmountInCents)
+        {
+            int[] definitionList = new int[toatlAmountInCents + 1]; //Define a temp list
+            definitionList[0] = 1; //Assign first element of the list as 1
+
+            foreach (int coinItem in coinList) //Traverse the list
+            {
+                for (int i = coinItem; i <= toatlAmountInCents; i++)
+                {
+                    definitionList[i] += definitionList[i - coinItem]; //Increase and assign temp list's elements according to i and current coinItem
+                }
+            }
+
+            return definitionList[toatlAmountInCents];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Coins;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conins
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            int[] coinList = { 1, 5, 10, 25 }; // Penny, Nickel, Dime, Quarter
+            int toatlAmountInCents = 100; // 1 dollar = 100 cents
+
+            int changeWays = Coin.CountChanges(coinList, toatlAmountInCents);
+
+            Console.WriteLine($"Number of ways to make change for $1: {changeWays}");
+
+            Console.ReadLine();
+        }
+    }
+}
